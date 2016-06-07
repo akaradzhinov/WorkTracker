dae4344f6d6be3ac3a88c59524d697ab2c220d37
Aleksandar Karadzhinov <aleksandar.karadjinov@gmail.com>
2016-06-07 17:48:33 +0300

    Initial structure of the project including spring-boot specific files

---
+++ src/main/java/bg/sofia/tu/WorkTrackerApplication.java
@@ -0,0 +1,12 @@
+package bg.sofia.tu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTrackerApplication.class, args);
+	}
+}
